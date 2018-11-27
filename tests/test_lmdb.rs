3f75a4b3d38d662e0bef84c78e90b9e6c39b4770
jiangfb <jayphbee@163.com>
2018-11-28 01:21:58 +0800

    multiply read transaction tests with multiply threads

--- tests/test_lmdb.rs
+++ tests/test_lmdb.rs
@@ -24,47 +24,79 @@ use pi_db::db::{
 use pi_store::lmdb_file::{LmdbTable, LmdbWareHouse};
 
 #[test]
-fn test_create_table_and_transaction() {
+fn test_create_transaction() {
+    // https://www.openldap.org/lists/openldap-devel/201409/msg00001.html
+    // lmdb-sys/lmdb/libraries/liblmdb/mdb.c +2687
+    let tab = LmdbTable::new(&Atom::from("test"));
+    
+    let tab = Arc::new(tab);
+    let t1 = tab.clone();
+    let t2 = tab.clone();
+    let t3 = tab.clone();
+    
+    thread::spawn(move || {        
+        t1.transaction(&Guid(0), false);
+    });
+    thread::sleep_ms(1000);
+
+    thread::spawn(move || {
+
+        t2.transaction(&Guid(0), false);
+    });
+    thread::sleep_ms(1000);
+
+    thread::spawn(move || {
+        t3.transaction(&Guid(0), true);
+    });
+    thread::sleep_ms(1000);
+}
+
+#[test]
+fn test_get_put_iter() {
     let worker_pool0 = Box::new(WorkerPool::new(3, 1024 * 1024, 1000));
     worker_pool0.run(STORE_TASK_POOL.clone());
 
     let tab = LmdbTable::new(&Atom::from("test"));
     let tab2 = LmdbTable::new(&Atom::from("test2"));
-    // let txn = tab.transaction(&Guid(0), false);
+    // let txn = tab.transaction(&Guid(0), true);
     // let txn2 = tab.transaction(&Guid(1), false);
-    // let txn3 = tab.transaction(&Guid(2), true);
-    let txn4 = tab.transaction(&Guid(3), true);
 
-    let tab_name = Atom::from("player");
-    let ware_name = Atom::from("file_test");
+    let tab = Arc::new(tab);
 
-    let key1 = Arc::new(Vec::from(String::from("key1").as_bytes()));
-    let value1 = Arc::new(Vec::from(String::from("value1").as_bytes()));
-    let key2 = Arc::new(Vec::from(String::from("key2").as_bytes()));
-    let value2 = Arc::new(Vec::from(String::from("value2").as_bytes()));
-    let key3 = Arc::new(Vec::from(String::from("key3").as_bytes()));
-    let value3 = Arc::new(Vec::from(String::from("value3").as_bytes()));
-
-    let item1 = create_tabkv(ware_name.clone(), tab_name.clone(), key1.clone(), 0, Some(value1.clone()));
-    let item2 = create_tabkv(ware_name.clone(), tab_name.clone(), key2.clone(), 0, Some(value2.clone()));
-    let item3 = create_tabkv(ware_name.clone(), tab_name.clone(), key3.clone(), 0, Some(value3.clone()));
-    let items =  Arc::new(vec![item1.clone(), item2.clone(), item2.clone()]);
-
-    txn4.modify(items.clone(), None, false, Arc::new(move |modify| {
-        assert!(modify.is_ok());
-        println!("{:?}", modify);
-    }));
+    // let txn3 = tab.transaction(&Guid(2), false);
     thread::sleep_ms(1000);
-    txn4.query(items.clone(), None, false, Arc::new(move |query| {
-        assert!(query.is_ok());
-        println!("{:?}", query);
-    }));
-    thread::sleep_ms(1000);
-    txn4.iter(None, false, None, Arc::new(move |it| {
-        assert!(it.is_ok());
-    }));
-    thread::sleep_ms(1000);
-    // println!("{:?}", tab);
+    // let txn4 = tab.transaction(&Guid(3), false);
+
+    // let tab_name = Atom::from("player");
+    // let ware_name = Atom::from("file_test");
+
+    // let key1 = Arc::new(Vec::from(String::from("key1").as_bytes()));
+    // let value1 = Arc::new(Vec::from(String::from("value1").as_bytes()));
+    // let key2 = Arc::new(Vec::from(String::from("key2").as_bytes()));
+    // let value2 = Arc::new(Vec::from(String::from("value2").as_bytes()));
+    // let key3 = Arc::new(Vec::from(String::from("key3").as_bytes()));
+    // let value3 = Arc::new(Vec::from(String::from("value3").as_bytes()));
+
+    // let item1 = create_tabkv(ware_name.clone(), tab_name.clone(), key1.clone(), 0, Some(value1.clone()));
+    // let item2 = create_tabkv(ware_name.clone(), tab_name.clone(), key2.clone(), 0, Some(value2.clone()));
+    // let item3 = create_tabkv(ware_name.clone(), tab_name.clone(), key3.clone(), 0, Some(value3.clone()));
+    // let items =  Arc::new(vec![item1.clone(), item2.clone(), item2.clone()]);
+
+    // txn4.modify(items.clone(), None, false, Arc::new(move |modify| {
+    //     assert!(modify.is_ok());
+    //     println!("{:?}", modify);
+    // }));
+    // thread::sleep_ms(1000);
+    // txn4.query(items.clone(), None, false, Arc::new(move |query| {
+    //     assert!(query.is_ok());
+    //     println!("{:?}", query);
+    // }));
+    // thread::sleep_ms(1000);
+    // txn4.iter(None, false, None, Arc::new(move |it| {
+    //     assert!(it.is_ok());
+    // }));
+    // thread::sleep_ms(1000);
+    // // println!("{:?}", tab);
 }
 
 #[test]
