64ed5d722481cdb60e7a57f219d45f0513da9a9f
jiangfb <jayphbee@163.com>
2021-03-23 16:55:40 +0800

    remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
 #[allow(dead_code,unused_variables,non_snake_case,unused_parens,unused_assignments,unused_unsafe,unused_imports)]
 
-#[macro_use]
-extern crate lazy_static;
-#[macro_use]
-extern crate log;
-
 pub mod log_store;
 pub mod log_file_db;
